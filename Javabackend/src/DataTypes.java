21b49faa75551be2d147e26f775f08cd4c20c908
Shiva Sai Ganesh <Shiva Sai Ganesh@SSG>
2025-08-16 18:49:11 +0530

    object

---
+++ Javabackend/src/DataTypes.java
@@ -0,0 +1,11 @@
+
+public class DataTypes {
+	int value1 = 8/2;// integer division 
+	float value2 = 101/65;
+	double value3 = 101/67;
+	float value4 = 5/2.5f;//float division
+	long value5 = 9000/56;
+	String value6 = "shivasaiganesh";
+	
+
+}
---
+++ Javabackend/src/DataTypesDemo.java
@@ -0,0 +1,16 @@
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		 DataTypes  obj = new DataTypes();
+		 System.out.println(obj.value1);
+		 System.out.println(obj.value2);
+		 System.out.println(obj.value3);
+		 System.out.println(obj.value4);
+		 System.out.println(obj.value5);
+		 System.out.println(obj.value6);
+
+
+	}
+
+}
