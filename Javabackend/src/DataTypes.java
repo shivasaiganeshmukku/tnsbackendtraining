f861c3a365f194b1377b508133c72034922ccb84
Shiva Sai Ganesh <Shiva Sai Ganesh@SSG>
2025-08-16 19:38:24 +0530

    object

--- Javabackend/src/DataTypes.java
+++ Javabackend/src/DataTypes.java
@@ -1,11 +1,41 @@
 
 public class DataTypes {
+	//byte takes 1 byte = 127 to - 127
+		//short -2bytes = 32767 to -32768
+		//int - 4bytes = 2147483647 to -2147483648
+		//long - 8bytes = 9223372036854775807L to 9223372036854775808L
+		//float -f = 3234.141243278345f
+		//double- d = 3456.14124512345678902345678914f
+		//boolean = True or False
+		//char demo
+		//assigning single character literal
 	int value1 = 8/2;// integer division 
 	float value2 = 101/65;
 	double value3 = 101/67;
 	float value4 = 5/2.5f;//float division
 	long value5 = 9000/56;
 	String value6 = "shivasaiganesh";
+	/*
+	 * identifiers 
+	 * int number 2 = 13; //error because do not add space within identifier
+	  int for = 13;//error because keyword cannot be identifier
+	  */
+	//error because identifier can not start with @,#
+	//int @number3 = 10;
+	//Valid identifier 
+	int $number = 20;//starts with$
+	int marks = 520;
+	//assigning expression to variable
+	double percentage = marks*0.5f;
+	
+	char ss = 'a';
+	//assigning number to a char 
+	char sg = 65;//in the char number shows the asci value 65 value is A
+	//type casting 
+	byte b = 10 ;
+	int i = b;
+	char ch = 'A';
+	int i3 = ch;//the char value A is 65
 	
 
 }
--- Javabackend/src/DataTypesDemo.java
+++ Javabackend/src/DataTypesDemo.java
@@ -3,12 +3,18 @@ public class DataTypesDemo {
 
 	public static void main(String[] args) {
 		 DataTypes  obj = new DataTypes();
-		 System.out.println(obj.value1);
+		 System.out.println("value1 = " + obj.value1);
 		 System.out.println(obj.value2);
 		 System.out.println(obj.value3);
 		 System.out.println(obj.value4);
 		 System.out.println(obj.value5);
 		 System.out.println(obj.value6);
+		 System.out.println("value of the number id :" + obj.$number);
+		 System.out.println("percentage:"+ obj.percentage);
+		 System.out.println("char single literal is :" + obj.ss);
+		 System.out.println("char number value is :" + obj.sg);
+		 System.out.println("Type casting:"+obj.i);
+		 System.out.println("char type casting:"+obj.i3);
 
 
 	}
